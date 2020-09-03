8a610fdf7e96c5dae88fe8a28948c8ac96c1f415
mford99 <39373767+mford99@users.noreply.github.com>
2020-09-03 17:39:19 -0230

    Add files via upload

---
+++ Question1.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import numpy as np
+import matplotlib
+
+DataScienceCSV = pd.read_csv("DataScience.csv")
+
+print(DataScienceCSV["order_amount"].describe())
+
+print("\nThe median is: " + str(DataScienceCSV["order_amount"].median()))
+
+print(DataScienceCSV["order_amount"].quantile(q=0.9))
+
+q1 = DataScienceCSV["order_amount"].quantile(q=0.25)
+q2 = DataScienceCSV["order_amount"].quantile(q=0.5)
+q3 = DataScienceCSV["order_amount"].quantile(q=0.75)
+IQR = q3 - q1
+
+FilteredData = DataScienceCSV[(DataScienceCSV["order_amount"] < q2 + IQR * 2) & (DataScienceCSV["order_amount"] > q2 - IQR * 2)]
+print("The filered data is:\n")
+print(FilteredData["order_amount"].describe())
+
+print(FilteredData["order_amount"].median())
